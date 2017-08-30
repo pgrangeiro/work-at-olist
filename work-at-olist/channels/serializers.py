20218c80fea45c73f15d57d20a4a69081f4b1a3d
Paula Grangeiro <contato@paulagrangeiro.com.br>
2017-08-29 23:07:11 -0300

    Add Category serializers

--- work-at-olist/channels/serializers.py
+++ work-at-olist/channels/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from channels.models import Channel
+from channels.models import Category, Channel
 
 
 class ChannelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Channel
-        fields = ('name',)
+        fields = ('name', )
+
+
+class CategoryResumeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    parent = CategoryResumeSerializer()
+    subcategories = CategoryResumeSerializer(many=True, source='category_set')
+
+    class Meta:
+        model = Category
+        fields = ('name', 'parent', 'subcategories')
--- work-at-olist/channels/tests/test_serializers.py
+++ work-at-olist/channels/tests/test_serializers.py
@@ -1,7 +1,9 @@
 import pytest
 from model_mommy import mommy
 
-from channels.serializers import ChannelSerializer
+from channels.serializers import (
+    CategorySerializer, CategoryResumeSerializer, ChannelSerializer
+)
 
 
 @pytest.mark.django_db
@@ -15,3 +17,59 @@ class TestChannelSerializer:
         data = ChannelSerializer(self.instance).data
 
         assert self.instance.name == data['name']
+
+
+@pytest.mark.django_db
+class TestCategoryResumeSerializer:
+
+    @pytest.fixture(autouse=True)
+    def set_fixtures(self):
+        self.instance = mommy.make('channels.Category')
+
+    def test_serializes_instance_correctly(self):
+        data = CategoryResumeSerializer(self.instance).data
+
+        assert self.instance.name == data['name']
+
+
+@pytest.mark.django_db
+class TestCategorySerializer:
+
+    @pytest.fixture(autouse=True)
+    def set_fixtures(self):
+        self.parent = mommy.make('channels.Category')
+        self.category = mommy.make('channels.Category')
+        self.subcategories = mommy.make('channels.Category', _quantity=2)
+
+    def test_serializes_instance_correctly(self):
+        data = CategorySerializer(self.category).data
+
+        assert self.category.name == data['name']
+        assert not data['parent']
+        assert not data['subcategories']
+
+    def test_serializes_instance_with_parent_correctly(self):
+        self.parent.category_set.add(self.category)
+        self.parent.save()
+
+        data = CategorySerializer(self.category).data
+        expected_parent = CategoryResumeSerializer(self.parent).data
+
+        assert self.category.name == data['name']
+        assert expected_parent == data['parent']
+        assert not data['subcategories']
+
+    def test_serializes_instance_with_subcategories_correctly(self):
+        for c in self.subcategories:
+            self.category.category_set.add(c)
+            self.category.save()
+
+        data = CategorySerializer(self.category).data
+        expected_subcategories = CategoryResumeSerializer(
+            self.subcategories, many=True
+        ).data
+
+        assert self.category.name == data['name']
+        assert not data['parent']
+        assert 2 == len(data['subcategories'])
+        assert expected_subcategories == data['subcategories']
